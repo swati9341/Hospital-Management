9315e1b6beed28c263323892cbb51f96e4960e0c
swati9341 <swati30092004@gmail.com>
2025-04-17 22:54:25 +0530

    Doctor Class Added

--- hospital-management-system.cpp
+++ hospital-management-system.cpp
@@ -1,65 +1,107 @@
 #include<iostream>
 #include<string>
+
 using namespace std;
-class person{
+class Person{
 public:
     int age;
     string name;
     string address;
     string email;
-    int phoneno;
-     void personinfo(){
-        cout<<"\n enter age of the patient";
+    long long PhoneNo;
+     void inputPersonDetails(){
+        cout<<"\n Enter age of the patient:";
         cin>>age;
-        cout<<"\n enter name of the patient";
+        cout<<"\n Enter name of the patient:";
         cin>>name;
-        cout<<"\n enter address of the patient:";
+        cout<<"\n Enter address of the patient:";
         cin>>address;
-        cout<<"\n enter email of the patient";
+        cout<<"\n Enter email of the patient:";
         cin>>email;
-        cout<<"\n enter phoneno of the patient";
-        cin>>phoneno;
+        cout<<"\n Enter phoneno of the patient:";
+        cin>>PhoneNo;
 
     }
-    void display(){
-        cout<<"\n age:"<<age<<"\n name: "<<name<<" \n address: "<<address<<" \n phoneno "<<phoneno;
+    void displayPersonDetails(){
+        cout<<"\n Age:"<<age<<"\n Name: "<<name<<" \n Address: "<<address<<" \n PhoneNo "<<PhoneNo;
     }
 
 };
 class Disease{
 public:
-    string disease;
-    void getdiseaseinfo(){
-    cout<<"\n enter the name of the disease:";
-    cin>> disease;
+    string diseasename;
+    void inputDisease(){
+    cout<<"\n Enter the name of the disease:";
+    cin>> diseasename;
     }
-    void showdisease(){
-        cout<<"\n disease :"<<disease;
+    void displayDisease(){
+        cout<<"\n Disease :"<<diseasename;
     }
 };
-class patient :public person,Disease{
+class Patient :public Person
+{
 public:
-    patient(){
-    cout<<"\n enter the patient record:";
+    Disease disease;
+        string appointmentDate;
+        string appointmentTime;
+
+    void inputPatientDetails(){
+    cout<<"\n  --- Enter the patient record ---";
+    inputPersonDetails();
+    disease.inputDisease();
+    cout<<"Enter appointment date (dd/mm/yyyy):";
+   cin>>appointmentDate;
+    cout<<"Enter appointment time(hh:mm)";
+    cin>>appointmentTime;
+    } 
+    void displayPatientDetails(){
+    cout<<"\n--- Patient Information--";
+    displayPersonDetails();
+    cout<<"\n  Appointment Date:"<<appointmentDate;
+    cout<<"\n  Appointment Time:"<<appointmentTime;
     }
     };
-    class Availabeon{
-    public:
-        double data;
-        double time;
-        string
+
+    class Doctor:public Person
+    {
+        public:
+    string shiftTime;
+          int doctorid;
+          int roomno;
+
+         void  Doctorassign(){
+            cout<<"\n\n Doctor assigned to the patient";
+            cout<<" \n Doctor id";
+            cin>>doctorid;
+            cout<<"\n  Room no";
+            cin>>roomno;
+            cout<<"\n ShiftTime:";
+            cin>>shiftTime;
+          }
+          void displayDoctorDetails(){
+            cout<<"\n ---Doctor Information---";
+            cout<<"\n Doctor id:"<<doctorid;
+            cout<<"\n Room no"<<roomno;
+            cout<<"\n Time to meeet :"<< shiftTime;
+          }
+
     };
-    class Doctor{
+    class Receptionist{
+    public:
+         void Receptionistwork(){
+        Patient patient;
+        Doctor doctor;
+        patient.inputPatientDetails();
+        doctor.Doctorassign();
+        cout<<"\n\n=== Hospital Management Record ===";
+        patient.displayPatientDetails();
+        doctor.displayDoctorDetails();
+        }
     };
 int main(){
-    patient p;
-person p1;
-p1.personinfo();
-
-Disease d;
-d.getdiseaseinfo();
-p1.display();
-d.showdisease();
-
+    Receptionist r;
+    r.Receptionistwork();
 return 0;
 }
+
+
